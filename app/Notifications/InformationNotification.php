1ba460a549992e353928813abfb632fcc70e9ceb
MIstrut <mo.ichisawa@gmail.com>
2023-07-31 17:09:45 +0900

    slack code rewrite . vendor remake

--- app/Notifications/InformationNotification.php
+++ app/Notifications/InformationNotification.php
@@ -5,6 +5,7 @@ namespace App\Notifications;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Messages\SlackMessage;
 use Illuminate\Notifications\Notification;
 use App\Models\Information;
 
@@ -13,6 +14,9 @@ class InformationNotification extends Notification
     use Queueable;
     
     private Information $information;
+    protected $channel;
+    protected $icon;
+    protected $name;
 
     /**
      * Create a new notification instance.
@@ -22,6 +26,9 @@ class InformationNotification extends Notification
     public function __construct(Information $information)
     {
         $this->information = $information;
+        $this->channel = config('slack.channel');
+        $this->icon = config('slack.icon');
+        $this->name = config('slack.sender_name');
     }
 
     /**
@@ -32,7 +39,7 @@ class InformationNotification extends Notification
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['database','mail','slack'];
     }
 
     /**
@@ -50,5 +57,23 @@ class InformationNotification extends Notification
              //  通知からリンクしたいURLがあれば設定しておくと便利
              //'url' => route('infos.show', ['information' => $this->information])
         ];
+    }
+    
+     public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->greeting('個人情報が変更されました')
+            ->line('サイト名：' . $this->information->site_name)
+            ->line('サイトURL：' . $this->information->site_url);
+    }
+    
+    public function toSlack($notifiable)
+    {
+        return (new SlackMessage)
+            ->from($this->name)
+            ->to($this->channel)
+            ->content('個人情報が変更されました')
+            ->content('サイト名：' . $this->information->site_name)
+            ->content('サイトURL：' . $this->information->site_url);
     }
 }
